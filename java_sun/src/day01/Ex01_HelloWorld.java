1fea4cc77d2e6dc3dfdafa483bcc4850dee9ccf6
Sunil <umsunil2@naver.com>
2024-12-30 12:38:26 +0900

    자바 1일차 helloworld

---
+++ java_sun/src/day01/Ex01_HelloWorld.java
@@ -0,0 +1,18 @@
+package day01;
+// 한줄 주석
+/* 여러줄 주석 */
+public class Ex01_HelloWorld {
+	/*
+	 * 메소드 설명할 때 사용
+	 * @param args 뭔지는 몰라요
+	 * */
+	// 프로그램이 실행되려면 main이 있어야 함.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 들여쓰기는 Tap
+		// 내여쓰기는 Shift + Tap
+		System.out.println("Hello World!");
+		
+	}
+
+}
