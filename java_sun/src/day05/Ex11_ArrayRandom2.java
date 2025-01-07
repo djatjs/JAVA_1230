6b7c101ad6979f36d4d227a335370ba099eb0bc5
Sunil <umsunil2@naver.com>
2025-01-07 10:12:28 +0900

    자바 5일차

--- java_sun/src/day05/Ex11_ArrayRandom2.java
+++ java_sun/src/day05/Ex11_ArrayRandom2.java
@@ -5,60 +5,80 @@ import java.sql.ResultSet;
 public class Ex11_ArrayRandom2 {
 
 	public static void main(String[] args) {
-		/* 1~10 사이의 중복되지 않은 랜덤한 수 6개를 배열에 저장하는 코드
-		 * */
-		int min =1, max=10, count=0;
-		int [] arr = new int[6];
-		
-		while(count<arr.length) {
-			int num = (int)(Math.random() * (max - min + 1) + min);
+		/*
+		 * 1~10 사이의 중복되지 않은 랜덤한 수 6개를 배열에 저장하는 코드
+		 */
+		int min = 1, max = 10, count = 0;
+		int[] arr = new int[6];
+
+		while (count < arr.length) {
+			int num = (int) (Math.random() * (max - min + 1) + min);
 			boolean result = false;
-			for (int i=0; i<arr.length;i++) {
-				if(num==arr[i]) {
+
+			for (int i = 0; i < arr.length; i++) {
+				if (num == arr[i]) {
 					result = true;
 					break;
 				}
 			}
-			if(result) {
+
+			if (result) {
 				continue;
-			}
-			else {
-				arr[count]=num;
+			} else {
+				arr[count] = num;
 				count++;
 			}
 		}
-		for(int i = 0; i<arr.length; i++) {
+		printArray(arr);
+		/*
+		 * for(int i = 0; i<arr.length; i++) { System.out.print(arr[i] + " "); }
+		 * System.out.println();
+		 */
+		System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+		
+		int[] arr2 = createRandomArray(min, max, 6);
+		printArray(arr2);
+
+	}
+	
+	// 중복 없는 랜덤 배열 만들기
+	public static int[] createRandomArray(int min, int max, int size) {
+		if (size <= 0) {
+			return null;
+		}
+
+		int[] arr = new int[size];
+		int count = 0;
+		while (count < arr.length) {
+			int num = (int) (Math.random() * (max - min + 1) + min);
+			boolean result = false;
+
+			if (contains(arr, num)) {// 포함되어 있다면
+				continue; // 넘기셈
+			} else {
+				arr[count] = num;
+				count++;
+			}
+		}
+		return arr;
+	}
+	
+	// 배열 숫자 나열하기
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
-
 	}
-	public static void printArray(int[]arr) {
-		for (int i=0; i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}System.out.println();
-	}
-	//아니 메소드로 못바꾸겠음;;;;;;;;;;;;;
 	
-	public static int[] createArray(int min, int max, int count) {
-		int count1 =0;
-		int [] array = new int[6]; 
-		while(count1 < array.length) {
-			boolean result = false;
-			int num = (int)(Math.random() * (max - min + 1) + min);
-			
-			for (int i=0; i<array.length;i++) {
-				if(array[i]==num) {
-					result = true;
-					break;
-				}
-			}if(result) {
-				continue;
-			}else {
-				array[count1]=num;
-				count1++;
+	// num이 arr배열에 속해있는지 확인
+	public static boolean contains(int[] arr, int num) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return true;
 			}
-		}return array;
+		}
+		return false;
 	}
 
 }
