275edf1dda875b5b5a5dce0c178041a4b0b68fb8
Sunil <umsunil2@naver.com>
2025-01-07 18:57:44 +0900

    자바 5일차

--- java_sun/src/day05/Ex12_Lotto.java
+++ java_sun/src/day05/Ex12_Lotto.java
@@ -16,60 +16,63 @@ public class Ex12_Lotto {
 		 * 5등 : 3개 일치 
 		 * 꽝 : 나머지
 		 */
-
-		int[] lotto = new int[7];
-		int bonus = 0; // 이건 배열에 저장된거랑도 안겹쳐야됨
+		
 		int min = 1, max = 45;
-
-		int count = 0;
-		while (count < lotto.length - 1) {
-			Boolean result = false;
-			int num = (int) (Math.random() * (max - min + 1) + min);
-			for (int i = 0; i < lotto.length - 1; i++) { // 중복 확인
-				if (num == lotto[i]) {
-					result = true; // 겹친다면
-					break;
-				} // else는 위에 이미 false로 설정해둬서 필요없음
-			}
-			if (result) {// 포함한다면
-				// continue;
-			} else {
-				lotto[count] = num;
-				count++;
-			}
-		}
-		while (lotto[6] ==0) {
-			Boolean result = false;
-			bonus = (int) (Math.random() * (max - min + 1) + min);
-			for (int i = 0; i < lotto.length - 1; i++) {
-				if (bonus == lotto[i]) {
-					result = true; // 겹친다면
-					break;
-				}
-
-			}
-			if (result) {// 포함한다면
-				// continue;
-			} else {
-				count++;
-				lotto[6] = bonus;
-			}
-
-		}
+		
+		// 로또 배열 만들기
+		int[] lotto = Ex11_ArrayRandom2.createRandomArray(min, max, 6);
+		
 		// 일단 출력
-		for (int i = 0; i < lotto.length; i++) {
-			System.out.print(lotto[i]+" ");
-		}
-		System.out.println();
+		Ex11_ArrayRandom2.printArray(lotto);
+
+		// 마지막 7번째 숫자 : 배열 안에 없는 숫자일 때까지 반복
+		int bonus = 0;
+		do{
+			bonus = (int) (Math.random() * (max - min + 1) + min);
+		}while(Ex11_ArrayRandom2.contains(lotto, bonus));
+		
+		// 일단 출력22
+		System.out.println("보너스 : " + bonus);
 		
 		// 입력 받기
 		Scanner scan = new Scanner(System.in);
 		int[] myNum = new int[6];
-		System.out.println("1~45까지 숫자를 입력해주세요");
+		System.out.println("로또 숫자를 입력해주세요(1~45)");
 		for (int i=0; i<myNum.length;i++) {
 			System.out.print((i+1)+"번째 숫자 입력 : ");
 			myNum[i]=scan.nextInt();
 			System.out.println(myNum[i]+" ");
+		}
+		
+		//랜덤 번호와 입력 번호의 일치하는 개수를 셈
+		int count=0;
+		for (int i = 0; i < lotto.length; i++) {
+			//로또에서 꺼내서 사용자 번호와 일치하는지 확인해서 일치하면 개수 증가
+			if(Ex11_ArrayRandom2.contains(lotto, myNum[i])){
+				count++;
+			}
+		}
+		
+		//일치하는 개수에 따라 등수 출력
+		switch(count) {
+		case 6:
+			System.out.println("1등!");
+			break;
+		case 5:
+			if(Ex11_ArrayRandom2.contains(myNum, bonus)) {
+				System.out.println("2등!");
+			}else {
+				System.out.println("3등!");
+			}
+			break;
+		case 4:
+			System.out.println("4등!");
+			break;
+		case 3:
+			System.out.println("5등!");
+			break;
+		default:
+			System.out.println("꽝!");
 		}
 
 	}
