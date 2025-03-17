b0813f0eb5b1750035cc0319058d34cef55d68d5
Sunil <umsunil2@naver.com>
2025-03-17 15:50:35 +0900

    스프링 01일차 : spring - 서버와 화면 데이터 주고 받는 예제

--- spring/framework/spring_sun/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/framework/spring_sun/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,10 +1,11 @@
 package kr.kh.spring.controller;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.kh.spring.model.dto.PersonDTO;
 
 /**
  * Handles requests for the application home page.
@@ -28,7 +29,9 @@ public class HomeController {
 	 * */
 	//@RequestMapping(value = "/", method = RequestMethod.GET)
 	@GetMapping(value = "/")
-	public String home(Locale locale, Model model) {
+	public String home(Model model, String name, Integer age) { // int는 null 허용 X
+		System.out.println("화면에서 보낸 이름 : "+ name);
+		System.out.println("화면에서 보낸 이름 : "+ age);
 		/* 화면에 데이터를 전송하는 방법
 		 * - Model 객체를 이용하여 전송
 		 * - addAttribute("화면에서 쓸 이름","데이터")
@@ -41,8 +44,20 @@ public class HomeController {
 		 * 	=> 뷰 리졸버 설정은 servlet-context.xml에 있음
 		 *  => 기본 뷰 리졸버에 의해 /WEB-INF/views/home.jsp가 완성되어
 		 *  	최종적으로 해당 jsp의 결과 화면을 클라이언트에 전송*/
-		
 		return "home";
 	}
+	/* 메소드의 매개변수에 객체를 넣어주면, 매핑이 되든 안되든 기본 생성자를 이용해서 객체를 만듦.
+	 * => 화면에서 보낸 변수의 이름과 같은 필드가 있으면 자동으로 매핑이 되어 값이 변경됨. 이 때 setter를 호출
+	 * */
+	// Get, Post 둘다 내용같아서 Request로 둠
+	// @GetMapping, @PostMapping에서 처리하는 내용이 같은 경우 @RequestMapping으로 묶을 수 있음
+	@RequestMapping(value = "/send") 
+	public String send(Model model, PersonDTO person) {
+		System.out.println("화면에서 보낸 이름과 나이 : "+ person);
+		/* 서버에서 화면으로 객체 전송 */
+		model.addAttribute("person", person);
+		return "sample/send";
+	}
+	
 	
 }
