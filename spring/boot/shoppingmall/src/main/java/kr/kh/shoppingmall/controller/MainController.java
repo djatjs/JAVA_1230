da789aeda9030e5bd2cd02ff07ca3f526ef8074d
Sunil <umsunil2@naver.com>
2025-04-28 17:51:38 +0900

    z

---
+++ spring/boot/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/MainController.java
@@ -0,0 +1,20 @@
+package kr.kh.shoppingmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+    @GetMapping("/signup")
+    public String signup() {
+        return "user/signup";
+    }
+    
+    
+}
